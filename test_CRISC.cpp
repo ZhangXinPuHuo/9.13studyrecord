9b2ead384f835c5a1a6a2b7353232a51df4898db
掌心扑火 <111477321+ZhangXinPuHuo@users.noreply.github.com>
2022-09-13 11:38:34 +0800

    Update test_CRISC.cpp

--- test_CRISC.cpp
+++ test_CRISC.cpp
@@ -1,4 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*Chicken and rabbit in the same cage 
+It is known that the total number of 
+chickens and rabbits is n and the total number of legs is m. 
+Input n and m, and output the number of chickens and rabbits in sequence. 
+If there is no solution, output no answer.*/
 #include <stdio.h>
 #include <math.h>
 int main() {
@@ -6,10 +10,12 @@ int main() {
 	scanf("%d %d", &n, &m);
 	x = 2*n- 0.5*m;
 	y = n - x;
-	if (m % 2 == 1 || x < 0 || y < 0)
+	if (m % 2 == 1 || x < 0 || y < 0){
 		printf("No answer\n");
-	else
+	}
+	else{
 		printf("%d %d", x, y);
+	}
 	return 0;
 }
 	
